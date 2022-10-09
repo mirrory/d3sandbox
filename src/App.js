a3537bb723d901649b9c63a4d33d47e3cfc4e533
Melissa Iori <miori.code@gmail.com>
2022-10-09 15:31:59 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import * as d3 from "d3";
+import {useEffect, useState} from 'react'
+import Renderer from './Renderer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h2 className="title">D3 Sandbox</h2>
+    <Renderer />
     </div>
   );
 }
